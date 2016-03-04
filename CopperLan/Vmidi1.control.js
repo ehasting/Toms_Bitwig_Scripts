afcf58b4f62e5184d9348c598f464f81a2c3c8c1
Thomas Helzle <ThomasHelzle@users.noreply.github.com>
2016-03-04 11:48:42 +0100

    Update Vmidi1.control.js

--- CopperLan/Vmidi1.control.js
+++ CopperLan/Vmidi1.control.js
@@ -50,7 +50,7 @@ function init() {
    VMidi15.setShouldConsumeEvents(false);
    VMidi16.setShouldConsumeEvents(false);
 
-	host.getMidiOutPort(0).setShouldSendMidiBeatClock;
+	host.getMidiOutPort(0).setShouldSendMidiBeatClock(true);
 
    host.getMidiInPort(0).setMidiCallback(onMidi);
 	//host.getMidiInPort(0).setSysexCallback(onSysex);
